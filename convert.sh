b87fe4e922c1f36e60031bfe31c49029ee67a9f5
Charles Averill <charlesaverill20@gmail.com>
2024-08-26 13:55:29 -0500

    Add highlight, only compile changed files

--- convert.sh
+++ convert.sh
@@ -7,7 +7,7 @@ cp -r templates/highlight docs/highlight
 python compile_html_template.py $1 templates/pd_template.html templates/style.css sub_template.html
 
 # Find all Markdown files
-md_files=($(find docs -type f -name "*.md"))
+md_files=($(git ls-files -m 'docs/**/*.md'))
 total_files=${#md_files[@]}
 
 # Function to display progress
